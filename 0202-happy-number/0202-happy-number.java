b03678c6854c1284c6b7513f2858d1db7783ee83
Peddinti_Kusuma <peddintikusuma@gmail.com>
2025-08-12 12:13:25 +0530

    Time: 2 ms (18.1%), Space: 41 MB (38.71%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+       Set<Integer> set=new HashSet<>();
+       while(n!=1 && !set.contains(n)){
+        set.add(n);
+        n=sumOfSquares(n);
+       }
+       return n==1;
+    }
+    public static int sumOfSquares(int num){
+        int sum=0;
+        while(num>0){
+            int digit=num%10;
+            sum+= digit*digit;
+            num/=10;
+        }
+        return sum;
+    }
+}
