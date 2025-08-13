b166bf12bdf07f76874a8e8a8f1d11e97a784964
Peddinti_Kusuma <peddintikusuma@gmail.com>
2025-08-13 12:17:46 +0530

    Time: 1 ms (100%), Space: 42.5 MB (75.14%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -8,9 +8,10 @@ class Solution {
             int digit1=(i>=0)? num1.charAt(i)-'0':0;
             int digit2=(j>=0)? num2.charAt(j)-'0':0;
             int sum=digit1+digit2+carry;
+                        sb.append(sum%10);
+                        
             carry=sum/10;
-            sb.append(sum%10);
-            i--;
+             i--;
             j--;
 
         }
