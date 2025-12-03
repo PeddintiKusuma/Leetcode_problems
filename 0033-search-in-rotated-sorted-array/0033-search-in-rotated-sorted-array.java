02441999cdedb11ad35bc7499d6dc7edce473f4c
PeddintiKusuma <peddintikusuma@gmail.com>
2025-12-03 11:18:16 +0530

    Time: 0 ms (100%), Space: 43.7 MB (36.52%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -2,24 +2,22 @@ class Solution {
     public int search(int[] nums, int target) {
         int left=0;
         int right=nums.length-1;
-        
         while(left<=right){
             int mid=(left+right)/2;
             if(nums[mid]==target){
                 return mid;
             }
-
             if(nums[left]<=nums[mid]){
-                if(nums[left]<=target && target<nums[mid]){
+                if(nums[left]<=target&&target<nums[mid]){
                     right=mid-1;
-
                 }
                 else{
                     left=mid+1;
                 }
             }
+
             else{
-                if(nums[mid]<target && target<=nums[right]){
+                if(nums[mid]<target&&nums[right]>=target){
                     left=mid+1;
                 }
                 else{
@@ -28,7 +26,5 @@ class Solution {
             }
         }
         return -1;
-         
-        
     }
 }
